5fdc5fd01a02d3bbe2953aa6403a1b615660a932
richardreiter <richard.reiter.dev@gmail.com>
2022-07-16 11:01:02 +0000

    Add functionality to add products to cart

--- cart/urls.py
+++ cart/urls.py
@@ -11,4 +11,5 @@ https://github.com/Code-Institute-Solutions/boutique_ado_v1
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
+    path('add(<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 """
@@ -13,3 +13,24 @@ def view_cart(request):
     A view to return the cart contents page
     """
     return render(request, 'cart/cart.html')
+
+
+
+def add_to_cart(request, item_id):
+    """
+    Add a quantity of the specified
+    product to the shopping cart
+    """
+    # get quantity from from
+    quantity = int(request.POST.get('quantity'))
+    # get redirect from form when process finished
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
