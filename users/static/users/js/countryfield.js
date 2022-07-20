e98bc948ca7b8f5ea5c1fd2635793eb7662b1c85
richardreiter <richard.reiter.dev@gmail.com>
2022-07-20 20:20:49 +0000

    Update profile template, add form and views

---
+++ users/static/users/js/countryfield.js
@@ -0,0 +1,16 @@
+// Many thanks to Chris Zielinski and CI's 'Boutique Ado'
+// Walkthrough project - a great reference, inspiration and example:
+// https://github.com/Code-Institute-Solutions/boutique_ado_v1
+
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#aab7c4');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#aab7c4');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
