a219c94733400d48faff48207132e89ce30f8b76
richardreiter <richard.reiter.dev@gmail.com>
2022-07-21 11:38:55 +0000

    Update the webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from users.models import UserProfile
 
 import json
 import time
@@ -46,6 +47,23 @@ class StripeWH_Handler:
             if value == "":
                 # replace empty strings with none
                 shipping_details.address[field] = None
+
+        # Update profile information if save_info was checked
+        # allow anonymous users to checkout
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         # if order doesn't exist
         order_exists = False
         attempt = 1
@@ -83,6 +101,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -94,7 +113,7 @@ class StripeWH_Handler:
                     original_cart=cart,
                     stripe_pid=pid,
                 )
-                # iterate thru bag items from json version in payment intent
+                # iterate thru cart items from json version in payment intent
                 for item_id, item_data in json.loads(cart).items():
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
