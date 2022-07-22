1b1cd0577586308adf2c3cedb331dfaa65c1b4c4
richardreiter <richard.reiter.dev@gmail.com>
2022-07-22 16:16:26 +0000

    Add a custom 404 page

--- melo_merch/urls.py
+++ melo_merch/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 """melo_merch URL Configuration
@@ -35,3 +36,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'melo_merch.views.handler404'
