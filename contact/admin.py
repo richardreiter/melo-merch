f4248d4666e5ffb4295771e4c13c85baebb1223b
richardreiter <richard.reiter.dev@gmail.com>
2022-07-23 14:25:05 +0000

    Register contact form in admin dash

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+
+class ContactFormAdmin(admin.ModelAdmin):
+    """
+    Register contact form
+    in admin panel
+    """
+
+    list_display = (
+        'name',
+        'email',
+        'subject',
+        'message',
+    )
+    # order by created on date
+    ordering = ('created_on',)
+
+
+admin.site.register(Contact, ContactFormAdmin)
