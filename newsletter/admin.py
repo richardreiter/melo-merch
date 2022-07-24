e2c78c9c9b33d67fe99a9dcab0464e86e285d5b2
richardreiter <richard.reiter.dev@gmail.com>
2022-07-24 13:26:23 +0000

    Add newsletter to admin panel

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import MailMessage, Subscribers
 
+
+"""
+Many thanks to Kenneth Broni for a
+great reference, inspiration and example:
+https://www.youtube.com/watch?v=hWtlskOaFNI
+"""
 # Register your models here.
+admin.site.register(MailMessage)
+admin.site.register(Subscribers)
