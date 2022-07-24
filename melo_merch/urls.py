e1c568860a27de2a3b6c2d44b95f25817dfeca99
richardreiter <richard.reiter.dev@gmail.com>
2022-07-24 14:36:19 +0000

    Add newsletter template, view and urls

--- melo_merch/urls.py
+++ melo_merch/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('users.urls')),
     path('contact/', include('contact.urls')),
+    path('newsletter/', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'melo_merch.views.handler404'
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .forms import SubscribersForm, MailMessageForm
+from .models import Subscribers
+from django.contrib import messages
+from django.core.mail import send_mail
 
+
+"""
+Many thanks to Kenneth Broni for a
+great reference, inspiration and example:
+https://www.youtube.com/watch?v=hWtlskOaFNI
+"""
 # Create your views here.
+
+
+def newsletter(request):
+    if request.method == 'POST':
+        form = SubscribersForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Subscription successful!')
+            return redirect(reverse('newsletter'))
+    else:
+        form = SubscribersForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'newsletter/newsletter.html', context)
+
