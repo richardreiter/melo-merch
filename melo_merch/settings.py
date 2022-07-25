9294065fa0ee7a817c521a63a9ffc72f51a7d82c
richardreiter <richard.reiter.dev@gmail.com>
2022-07-25 07:47:41 +0000

    Remove secret key and set debug

--- melo_merch/settings.py
+++ melo_merch/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-_6u!i+w$p6e*y&9-evri(=$fn3%%xb%z-7ork3)yj%d&imiho&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['melo-merch.herokuapp.com', 'localhost']
 
