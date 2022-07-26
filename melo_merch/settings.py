b51cb8e0beea9312491b77e73c52e3e2c364554c
richardreiter <richard.reiter.dev@gmail.com>
2022-07-26 10:07:23 +0000

    Send real emails

--- melo_merch/settings.py
+++ melo_merch/settings.py
@@ -246,6 +246,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+# send real emails
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'melomerch@example.com'
